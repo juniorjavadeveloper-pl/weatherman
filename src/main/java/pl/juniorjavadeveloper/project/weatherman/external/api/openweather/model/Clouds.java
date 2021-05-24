a197f4b6742b2ca26f0f0d84513b6fd7f1c64ca1
Junior Java Developer Mentor <kontakt@juniorjavadeveloper.pl>
2021-05-24 15:10:04 +0200

    Add generated POJOs (from https://www.jsonschema2pojo.org) for Current Weather Data Response

---
+++ src/main/java/pl/juniorjavadeveloper/project/weatherman/external/api/openweather/model/Clouds.java
@@ -0,0 +1,30 @@
+
+package pl.juniorjavadeveloper.project.weatherman.external.api.openweather.model;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+import javax.annotation.processing.Generated;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+    "all"
+})
+@Generated("jsonschema2pojo")
+public class Clouds {
+
+    @JsonProperty("all")
+    private Integer all;
+
+    @JsonProperty("all")
+    public Integer getAll() {
+        return all;
+    }
+
+    @JsonProperty("all")
+    public void setAll(Integer all) {
+        this.all = all;
+    }
+
+}
---
+++ src/main/java/pl/juniorjavadeveloper/project/weatherman/external/api/openweather/model/Coord.java
@@ -0,0 +1,43 @@
+
+package pl.juniorjavadeveloper.project.weatherman.external.api.openweather.model;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+import javax.annotation.processing.Generated;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+    "lon",
+    "lat"
+})
+@Generated("jsonschema2pojo")
+public class Coord {
+
+    @JsonProperty("lon")
+    private Double lon;
+    @JsonProperty("lat")
+    private Double lat;
+
+    @JsonProperty("lon")
+    public Double getLon() {
+        return lon;
+    }
+
+    @JsonProperty("lon")
+    public void setLon(Double lon) {
+        this.lon = lon;
+    }
+
+    @JsonProperty("lat")
+    public Double getLat() {
+        return lat;
+    }
+
+    @JsonProperty("lat")
+    public void setLat(Double lat) {
+        this.lat = lat;
+    }
+
+}
