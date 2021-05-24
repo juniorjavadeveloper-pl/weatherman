48af1320adc96fc2d18551a7e5e8dd53f52124e4
Junior Java Developer Mentor <kontakt@juniorjavadeveloper.pl>
2021-05-24 14:36:50 +0200

    Initial project commit

---
+++ src/main/java/pl/juniorjavadeveloper/project/weatherman/WeathermanApplication.java
@@ -0,0 +1,7 @@
+package pl.juniorjavadeveloper.project.weatherman;
+
+public class WeathermanApplication {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Weatherman Application!");
+    }
+}
